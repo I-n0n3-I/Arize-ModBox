8f57720be3cd2b93b8a49b81c9db8c08a0cdbcb6
/|_n0n3_| <shifatrifat1122@gmail.com>
2025-06-09 01:32:13 +0600

    Create Cooldown.lua module with initial structure

---
+++ src/Replicated/Storage/Managers/Cooldown.lua
@@ -0,0 +1,20 @@
+--|| Roblox Services ||--
+
+--<>--
+
+--|| Dependencies ||--
+
+--<>--
+
+--|| Infos ||--
+
+--<>--
+
+--|| Module ||--
+
+local Manager = {}
+local _class_Cooldown = {}
+
+return Manager
+
+--<>--
